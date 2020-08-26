3b491b8e470eae85c7f892b1fd19bfb859802743
Mummrik <joakim.lembke@futuregames.nu>
2020-08-26 17:49:59 +0200

    !A - Interaction Prompts

--- Source/HellGame/InteractableBase.h
+++ Source/HellGame/InteractableBase.h
@@ -5,34 +5,39 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Interactable.h"
+#include "HellGameHUD.h"
 #include "InteractableBase.generated.h"
 
 UCLASS()
 class HELLGAME_API AInteractableBase : public AActor, public IInteractable
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AInteractableBase();
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+	AHellGameHUD* HUD;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interactable")
 		void OnInteract(AActor* Caller);
-		virtual void OnInteract_Implementation(AActor* Caller);
+	virtual void OnInteract_Implementation(AActor* Caller);
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interactable")
 		void OnBeginFocus();
-		virtual void OnBeginFocus_Implementation();
+	virtual void OnBeginFocus_Implementation();
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interactable")
 		void OnEndFocus();
-		virtual void OnEndFocus_Implementation();
+	virtual void OnEndFocus_Implementation();
+
+	UPROPERTY(BlueprintReadWrite, Category = "Interactable")
+		bool bIsFocused;
 };
