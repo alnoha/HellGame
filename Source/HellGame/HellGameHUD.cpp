1f853e6b21881409aaf5b30435a027a14795a7ae
Mummrik <joakim.lembke@futuregames.nu>
2020-08-27 16:19:42 +0200

    !U - Changed Prompt system from hardcoded to using WidgetBlueprints

--- Source/HellGame/HellGameHUD.cpp
+++ Source/HellGame/HellGameHUD.cpp
@@ -9,6 +9,7 @@
 #include "SPromptPopup.h"
 #include "Widgets/SWeakWidget.h"
 #include "Engine/Engine.h"
+#include "Blueprint/UserWidget.h"
 
 AHellGameHUD::AHellGameHUD()
 {
@@ -28,28 +29,29 @@ void AHellGameHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y + 20.0f));
+	const FVector2D CrosshairDrawPosition((Center.X),
+		(Center.Y + 20.0f));
 
 	// draw the crosshair
-	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+	FCanvasTileItem TileItem(CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
 	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );
+	Canvas->DrawItem(TileItem);
 }
 
-void AHellGameHUD::ShowPrompt()
+void AHellGameHUD::ShowWidget(TSubclassOf<UUserWidget> Widget)
 {
-	if (GEngine && GEngine->GameViewport)
+	if (Widget != nullptr)
 	{
-		PromptWidget = SNew(SPromptPopup).OwningHUD(this);
-		GEngine->GameViewport->AddViewportWidgetContent(SAssignNew(PromptWidgetContainer, SWeakWidget).PossiblyNullContent(PromptWidget.ToSharedRef()));
+		PromptWidget = CreateWidget(GetWorld(), Widget);
+		PromptWidget->AddToViewport();
 	}
 }
 
-void AHellGameHUD::HidePrompt()
+void AHellGameHUD::HideWidget()
 {
-	if (GEngine && GEngine->GameViewport && PromptWidgetContainer.IsValid())
+	if (PromptWidget != nullptr)
 	{
-		GEngine->GameViewport->RemoveViewportWidgetContent(PromptWidgetContainer.ToSharedRef());
+		PromptWidget->RemoveFromViewport();
+		PromptWidget = nullptr;
 	}
 }
