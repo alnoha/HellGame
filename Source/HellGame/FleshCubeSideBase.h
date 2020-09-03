1612c9212b4a37b26d43e1a71c0938934a29a17f
Robin Ortryd <45140757+Haesst@users.noreply.github.com>
2020-09-03 11:41:47 +0200

    Added transform to remote activation signal

--- Source/HellGame/FleshCubeSideBase.h
+++ Source/HellGame/FleshCubeSideBase.h
@@ -45,8 +45,8 @@ public:
 	virtual void ReceivedStopSignal_Implementation();
 
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Activation signals")
-	void ReceiveRemoteActivationSignal();
-	virtual void ReceiveRemoteActivationSignal_Implementation();
+	void ReceiveRemoteActivationSignal(FTransform SideTransform);
+	virtual void ReceiveRemoteActivationSignal_Implementation(FTransform SideTransform);
 
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	void SetupMesh(UStaticMeshComponent* parent);
