9806402ee056dfce6fe4616d091b02da7583c591
Robin Ortryd <45140757+Haesst@users.noreply.github.com>
2020-09-03 13:32:42 +0200

    !U: Changed mesh component

--- Source/HellGame/FleshCube.cpp
+++ Source/HellGame/FleshCube.cpp
@@ -779,22 +779,22 @@ void AFleshCube::ReceiveRemoteActivationSignal(UStaticMeshComponent* SideMeshCom
 	{
 		if (CubeSide != LeftSide)
 		{
-			LeftSide->ReceivedActivationSignal(nullptr, ESideType::None, LeftSide->GetMeshComponent()->GetComponentToWorld(), true);
+			LeftSide->ReceivedActivationSignal(nullptr, ESideType::None, LeftSideMeshComponent->GetComponentToWorld(), true);
 		}
 
 		if (CubeSide != FrontSide)
 		{
-			FrontSide->ReceivedActivationSignal(nullptr, ESideType::None, FrontSide->GetMeshComponent()->GetComponentToWorld(), true);
+			FrontSide->ReceivedActivationSignal(nullptr, ESideType::None, FrontSideMeshComponent->GetComponentToWorld(), true);
 		}
 
 		if (CubeSide != RightSide)
 		{
-			RightSide->ReceivedActivationSignal(nullptr, ESideType::None, RightSide->GetMeshComponent()->GetComponentToWorld(), true);
+			RightSide->ReceivedActivationSignal(nullptr, ESideType::None, RightSideMeshComponent->GetComponentToWorld(), true);
 		}
 
 		if (CubeSide != BackSide)
 		{
-			BackSide->ReceivedActivationSignal(nullptr, ESideType::None, BackSide->GetMeshComponent()->GetComponentToWorld(), true);
+			BackSide->ReceivedActivationSignal(nullptr, ESideType::None, BackSideMeshComponent->GetComponentToWorld(), true);
 		}
 	}
 }
